66c3501a52b6beadfd303f40b59d4c16c4e7379c
Oshurkevich Roman <oshurkevichroman@yandex.ru>
2019-11-18 00:21:40 +0400

    shhhhhhh

--- backend/app/api/controllers/users.js
+++ backend/app/api/controllers/users.js
@@ -1,200 +1,253 @@
-const userModel = require('../models/users');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const userModel = require("../models/users");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-    create: async function (req, res, next) {
-        const { email, login, password } = req.body;
-        if (await userModel.findOne({ "email": req.body.email })) {
-            res.status(500).json({
-                message: 'email уже занят',
-                data: req.body.email
+  create: async function(req, res, next) {
+    const { email, login, password } = req.body;
+    if (await userModel.findOne({ email: req.body.email })) {
+      res.status(500).json({
+        message: "email уже занят",
+        data: req.body.email
+      });
+    } else if (await userModel.findOne({ login: req.body.login })) {
+      res.status(500).json({
+        message: "Имя уже занято",
+        data: req.body.login
+      });
+    } else
+      userModel.create(
+        {
+          login: login,
+          email: email,
+          password: password
+        },
+        function(err, result) {
+          if (err) {
+            res.status(500).json({ error: err });
+            next(err);
+          } else
+            res.status(200).json({
+              message: "Поздравляем, вы зарегистрированы!",
+              data: { login: req.body.login, email: req.body.email }
             });
         }
-        else if (await userModel.findOne({ "login": req.body.login })) {
-            res.status(500).json({
-                message: 'Имя уже занято',
-                data: req.body.login
-            });
-        }
-        else
-            userModel.create({
-                login: login,
-                email: email,
-                password: password
-            },
-                function (err, result) {
-                    if (err) {
-                        res.status(500).json({ error: err });
-                        next(err);
-                    }
-                    else
-                        res.status(200).json({
-                            message: 'Поздравляем, вы зарегистрированы!',
-                            data: { login: req.body.login, email: req.body.email }
-                        })
-                });
-    },
+      );
+  },
 
-    authenticate: function (req, res, next) {
-        userModel.findOne({ login: req.body.login }, function (err, userInfo) {
-            if (err) {
-                next(err);
-            } else {
-                if ((userInfo != null) && bcrypt.compareSync(req.body.password, userInfo.password)) {
+  authenticate: function(req, res, next) {
+    userModel.findOne({ login: req.body.login }, function(err, userInfo) {
+      if (err) {
+        next(err);
+      } else {
+        if (
+          userInfo != null &&
+          bcrypt.compareSync(req.body.password, userInfo.password)
+        ) {
+          const token = jwt.sign(
+            { id: userInfo._id },
+            req.app.get("secretKey"),
+            { expiresIn: "1h" }
+          );
 
-                    const token = jwt.sign({ id: userInfo._id }, req.app.get('secretKey'), { expiresIn: '1h' });
-
-                    res.status(200).json({
-                        status: 'успешно',
-                        message: 'Вы вошли!',
-                        data: { user: userInfo, token: token }
-                    });
-                }
-                else {
-                    res.status(500).json({
-                        status: 'error',
-                        message: 'Неправильный логин/пароль!',
-                    });
-                }
-            }
-        })
-    },
-
-    updateUserInfo: function (req, res, next) {
-        userModel.updateOne({ login: req.body.login }, { ...req.body.updateInfo }, function (err, result) {
-            if (err) {
-                res.status(500).json({ error: err });
-                next(err);
-            }
-            else {
-                res.status(200).json({ result, ...req.body.updateInfo })
-            }
-        })
-    },
-
-    getUserInfo: function (req, res, next) {
-        userModel.findOne({ login: req.body.login }, function (err, result) {
-            if (err) {
-                res.status(500).json({ error: err, message: 'Никого не найдено?' });
-                next(err);
-            }
-            else {
-                res.status(200).json({ user: result })
-            }
-        })
-    },
-
-    updateSettingsList: function (req, res, next) {
-        userModel.updateOne({ login: req.body.login }, { settingsList: req.body.settingsList }, function (err, result) {
-            if (err) {
-                res.status(500).json({ error: err, message: 'Не найден пользователь или не удалось обновить список настроек' });
-                next(err);
-            }
-            else {
-                res.status(200).json({ settingsList: req.body.settingsList });
-                next(result);
-            }
-        })
-    },
-
-    getSettingsList: function (req, res, next) {
-        if (req.body.login) {
-            userModel.findOne({ login: req.body.login }, function (err, result) {
-                if (err) {
-                    res.status(500).json({
-                        error: err,
-                        message: 'Не удалось получить данные настроек пользователя, загружаем стандартные'
-                    });
-                    next(err);
-                } else {
-                    res.status(200).json({ settingsList: result.settingsList })
-                }
-            })
-        }
-        else res.status(500).json({ message: 'Не удалось получить логин пользователя' })
-    },
-
-    searchFriends: function (req, res, next) {
-        if (req.body.name) {
-            userModel.find({ name: req.body.name }, function (err, result) {
-                if (err) {
-                    res.status(500).json({ message: `не найдено пользователей с именем ${req.body.name}` });
-                    next(err)
-                } else {
-                    //возвращаем id, name, surName найденных пользователей
-                    let peopleList = [...result].map(item => {
-                        return {
-                            id: item._id,
-                            name: item.name,
-                            surName: item.surName
-                        }
-                    });
-                    res.status(200).json({ peopleList })
-                }
-            })
+          res.status(200).json({
+            status: "успешно",
+            message: "Вы вошли!",
+            data: { user: userInfo, token: token }
+          });
         } else {
-            res.status(500).json({ message: `не задано имя` })
+          res.status(500).json({
+            status: "error",
+            message: "Неправильный логин/пароль!"
+          });
         }
-    },
+      }
+    });
+  },
 
-    sendFriendRequest: function (req, res, next) {
-        userModel.findById(req.body.id, function (err, result) {
-            if (err) {
-                res.status(500).json({ error: err, message: 'Не удалось найти профиль' });
-                next(err);
-            }
-            else {
-                const friendsOutRequests = [...result.friendsOutRequests];
-                if (friendsOutRequests.includes(req.body.friendId)) {
-                    res.status(500).json({ message: 'Вы уже отправили заявку этому пользователю' })
-                } else {
-                    userModel.findOneAndUpdate({ _id: req.body.id }, { friendsOutRequests: req.body.friendId }, (err, result) => {
-                        if (err) {
-                            res.status(500).json({ error: err, message: 'Произошла ошибка при попытке добавления в друзья' })
-                        } else {
-                            userModel.findOneAndUpdate({ _id: req.body.friendId }, { friendsInRequests: req.body.id }, function (err, result) {
-                                if (err) {
-                                    res.status(500).json({ err, message: 'Произошла ошибка при попытке добавления в друзья' })
-                                } else {
-                                    res.status(200).json({ message: 'Заявка на добавление в друзья отправлена' })
-                                }
-                            });
-                        }
-                    })
-                }
-            }
+  updateUserInfo: function(req, res, next) {
+    userModel.updateOne(
+      { login: req.body.login },
+      { ...req.body.updateInfo },
+      function(err, result) {
+        if (err) {
+          res.status(500).json({ error: err });
+          next(err);
+        } else {
+          res.status(200).json({ result, ...req.body.updateInfo });
         }
-        )
-    },
+      }
+    );
+  },
 
-    getFriendsInRequests: function (req, res, next) {
-        userModel.findById(req.body.id, function (err, result) {
-            if (err) {
-                res.status(500).json({ error: err, message: 'Не найден пользователь' });
-                next(err);
-            } else {
-                res.status(200).json({ friendsRequests: result.friendsInRequests });
-            }
-        })
-    },
+  getUserInfo: function(req, res, next) {
+    userModel.findOne({ login: req.body.login }, function(err, result) {
+      if (err) {
+        res.status(500).json({ error: err, message: "Никого не найдено?" });
+        next(err);
+      } else {
+        res.status(200).json({ user: result });
+      }
+    });
+  },
 
-    acceptFriendRequest: function (req, res, next) {
+  updateSettingsList: function(req, res, next) {
+    userModel.updateOne(
+      { login: req.body.login },
+      { settingsList: req.body.settingsList },
+      function(err, result) {
+        if (err) {
+          res.status(500).json({
+            error: err,
+            message:
+              "Не найден пользователь или не удалось обновить список настроек"
+          });
+          next(err);
+        } else {
+          res.status(200).json({ settingsList: req.body.settingsList });
+          next(result);
+        }
+      }
+    );
+  },
 
-        userModel.findByIdAndUpdate(req.body.id, { $push: { friends: { id: req.body.friendId } }, $pull: { friendsInRequests: req.body.friendId } }, function (err, result) {
-            if (err) {
-                res.status(500).json({ error: err, message: 'Не удалось принять заявку' });
-            } else if (result.friends) {
-            }
-        })
-            .then(() => userModel.findByIdAndUpdate(req.body.friendId, { $pull: { friendsOutRequests: req.body.id } }, (err, result) => {
-                if (err) {
-                    res.status(500).json({ error: err, message: 'Не удалось добавить пользователя' });
-                } else {
-                    res.status(200).json({ friendsList: result.friends, message: 'Пользователь добавлен в друзья' });
-                }
-            })
-            )
+  getSettingsList: function(req, res, next) {
+    if (req.body.login) {
+      userModel.findOne({ login: req.body.login }, function(err, result) {
+        if (err) {
+          res.status(500).json({
+            error: err,
+            message:
+              "Не удалось получить данные настроек пользователя, загружаем стандартные"
+          });
+          next(err);
+        } else {
+          res.status(200).json({ settingsList: result.settingsList });
+        }
+      });
+    } else
+      res
+        .status(500)
+        .json({ message: "Не удалось получить логин пользователя" });
+  },
+
+  searchFriends: function(req, res, next) {
+    if (req.body.name) {
+      userModel.find({ name: req.body.name }, function(err, result) {
+        if (err) {
+          res.status(500).json({
+            message: `не найдено пользователей с именем ${req.body.name}`
+          });
+          next(err);
+        } else {
+          //возвращаем id, name, surName найденных пользователей
+          let peopleList = [...result].map(item => {
+            return {
+              id: item._id,
+              name: item.name,
+              surName: item.surName
+            };
+          });
+          res.status(200).json({ peopleList });
+        }
+      });
+    } else {
+      res.status(500).json({ message: `не задано имя` });
     }
+  },
 
+  sendFriendRequest: function(req, res, next) {
+    userModel.findById(req.body.id, function(err, result) {
+      if (err) {
+        res
+          .status(500)
+          .json({ error: err, message: "Не удалось найти профиль" });
+        next(err);
+      } else {
+        const friendsOutRequests = [...result.friendsOutRequests];
+        if (friendsOutRequests.includes(req.body.friendId)) {
+          res
+            .status(500)
+            .json({ message: "Вы уже отправили заявку этому пользователю" });
+        } else {
+          userModel.findOneAndUpdate(
+            { _id: req.body.id },
+            { friendsOutRequests: req.body.friendId },
+            (err, result) => {
+              if (err) {
+                res.status(500).json({
+                  error: err,
+                  message: "Произошла ошибка при попытке добавления в друзья"
+                });
+              } else {
+                userModel.findOneAndUpdate(
+                  { _id: req.body.friendId },
+                  { friendsInRequests: req.body.id },
+                  function(err, result) {
+                    if (err) {
+                      res.status(500).json({
+                        err,
+                        message:
+                          "Произошла ошибка при попытке добавления в друзья"
+                      });
+                    } else {
+                      res.status(200).json({
+                        message: "Заявка на добавление в друзья отправлена"
+                      });
+                    }
+                  }
+                );
+              }
+            }
+          );
+        }
+      }
+    });
+  },
+
+  getFriendsInRequests: function(req, res, next) {
+    userModel.findById(req.body.id, function(err, result) {
+      if (err) {
+        res.status(500).json({ error: err, message: "Не найден пользователь" });
+        next(err);
+      } else {
+        res.status(200).json({ friendsRequests: result.friendsInRequests });
+      }
+    });
+  },
+
+  acceptFriendRequest: async function(req, res, next) {
+    const userUpdate = userModel.findByIdAndUpdate(
+      req.body.id,
+      {
+        $addToSet: { friends: { id: req.body.friendId } },
+        $pull: { friendsInRequests: req.body.friendId }
+      },
+      function(err, result) {
+        if (err) {
+          res
+            .status(500)
+            .json({ error: err, message: "Не удалось принять заявку" });
+        }
+      }
+    );
+    const friendUpdate = userModel.findByIdAndUpdate(
+      req.body.friendId,
+      { $pull: { friendsOutRequests: req.body.id } },
+      (err, result) => {
+        if (err) {
+          res.status(500).json({
+            error: err,
+            message: "Не удалось добавить пользователя"
+          });
+        } else {
+          res.status(200).json({
+            friendsList: result.friends,
+            message: "Пользователь добавлен в друзья"
+          });
+        }
+      }
+    );
+    await Promise.all([userUpdate, friendUpdate]);
+  }
 };
--- backend/app/api/models/users.js
+++ backend/app/api/models/users.js
@@ -1,65 +1,65 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 const UserSchema = mongoose.Schema({
-    login: {
+  login: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  name: {
+    type: String,
+    trim: true
+  },
+  surName: {
+    type: String,
+    trim: true
+  },
+  birthDate: {
+    type: Date,
+    trim: true
+  },
+  //0 - male, 1 - female
+  gender: {
+    type: Boolean
+  },
+  //list with activated user' settings
+  settingsList: {
+    type: Object
+  },
+  friends: [
+    {
+      id: {
         type: String,
-        trim: true,
-        required: true,
-    },
-    email: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    name: {
-        type: String,
-        trim: true
-    },
-    surName: {
-        type: String,
-        trim: true
-    },
-    birthDate: {
-        type: Date,
-        trim: true
-    },
-    //0 - male, 1 - female
-    gender: {
-        type: Boolean
-    },
-    //list with activated user' settings
-    settingsList: {
-        type: Object
-    },
-    friends: [
-        {
-            id: {
-                type: String,
-                unique: true
-            },
-        },
-    ],
-    friendsOutRequests: {
-        type: [String],
         unique: true
-    },
-    friendsInRequests: {
-        type: [String],
-        unique: true
+      }
     }
+  ],
+  friendsOutRequests: {
+    type: [String],
+    unique: true
+  },
+  friendsInRequests: {
+    type: [String],
+    unique: true
+  }
 });
 
 //hash user password before saving to database
-UserSchema.pre('save', function (next) {
-    this.password = bcrypt.hashSync(this.password, saltRounds);
-    next();
+UserSchema.pre("save", function(next) {
+  this.password = bcrypt.hashSync(this.password, saltRounds);
+  next();
 });
 
-module.exports = mongoose.model('User', UserSchema, 'users');
+module.exports = mongoose.model("User", UserSchema, "users");
